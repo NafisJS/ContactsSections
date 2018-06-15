e4a21bc94e506b63a5ea676439cb2767e9168e34
Nafis Shayakbarov <3344474@gmail.com>
2018-06-15 17:57:15 +0500

    Its begin of simple project - Contacts Sections

--- ContactsSections/ViewController.swift
+++ ContactsSections/ViewController.swift
@@ -8,18 +8,66 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UITableViewController {
+    
+    let cellId = "cellId"
+    let names = [
+        "Petya", "Vasya", "Ivan", "Masha", "Kolya"
+    ]
+    let cNames = [
+        "C_Alex", "C_Tolic", "C_Karl", "C_Mira", "C_Gosha"
+    ]
+    let dNames = [
+        "Dmitriy", "Daniil", "Dasha"
+    ]
+    let twoDimensionalArray = [
+        ["Petya", "Vasya", "Ivan", "Masha", "Kolya"],
+        ["C_Alex", "C_Tolic", "C_Karl", "C_Mira", "C_Gosha"],
+        ["Dmitriy", "Daniil", "Dasha"]
+    ]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        navigationItem.title = "Contacts"
+        navigationController?.navigationBar.prefersLargeTitles = true
+        
+        tableView.register(UITableViewCell.self, forCellReuseIdentifier: cellId)
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    override func tableView(_ tableView: UITableView, viewForHeaderInSection section: Int) -> UIView? {
+        let label = UILabel()
+        label.text = "Header"
+        label.backgroundColor = UIColor.lightGray
+        
+        return label
     }
-
+    
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        return twoDimensionalArray.count
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return twoDimensionalArray[section].count
+//        if section == 0 {
+//            return names.count
+//        }
+//        return cNames.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: cellId, for: indexPath)
+        
+//        let name = self.names[indexPath.row]
+        
+        let name = indexPath.section == 0 ? names[indexPath.row] : cNames[indexPath.row]
+        
+        cell.textLabel?.text = name
+        
+        cell.textLabel?.text = "\(name) Section: \(indexPath.section) Row: \(indexPath.row)"
+        
+        return cell
+    }
 
 }
 
